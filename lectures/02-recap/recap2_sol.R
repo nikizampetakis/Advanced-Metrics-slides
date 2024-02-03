07ad829c9dfd993a7de4370972a3aed666f30ad2
nikizampetakis <152624157+nikizampetakis@users.noreply.github.com>
2024-02-03 19:31:37 +0100

    Create recap2_sol.R

---
+++ lectures/02-recap/recap2_sol.R
@@ -0,0 +1,58 @@
+
+library(tidyverse)
+library(skimr)
+library(wooldridge)
+
+#Task 1
+
+data("wage1")
+
+wage1 %>% skim()
+
+summary(lm(wage~educ + tenure, data = wage1))
+
+#Task 2
+
+summary(lm(wage ~ female, data = wage1))
+
+summary(lm(wage ~ female + married, data = wage1))
+
+#Task 3
+
+lm1 = lm(wage ~ female*exper, data = wage1)
+
+summary(lm1)
+
+summary(lm(wage ~ exper + female + female:exper, data = wage1))
+
+male5 = tibble(female = 0, exper = 5)
+
+predict(lm1, male5)
+
+#Task 4
+
+summary(lm(lwage ~ educ + tenure, data = wage1))
+
+#Task 5
+
+data("hprice1")
+
+summary(lm(lprice ~ lsqrft, data = hprice1))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
